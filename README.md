4f60df0db99897735a6a9f7c55d41f190269b554
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-07-06 10:07:34 +0100

    initial

--- README.md
+++ README.md
@@ -21,3 +21,129 @@ A second version returns a nonzero process exit code if a DICOM file cannot not 
 RUN wget https://medistim.com/wp-content/uploads/2016/07/ttfm.dcm
 RUN python3 -c "o={};exec(\"import openslide;\nif openslide.OpenSlide.detect_format('ttfm.dcm') is not None:\n\tres='new openslide'\n\",globals(),o);print(o['res'])"
 ```
+
+## Maven
+
+### From scratch
+
+Visit [Using bio-formats as a Java library](https://bio-formats.readthedocs.io/en/latest/developers/java-library.html#bio-formats-as-a-java-library)
+which currently lists formats-gpl.jar as the main jar; `ome-common.jar` and `ome-xml.jar` and SLF4J, which is `slf4j-api` ([not than version 2 currently](https://bio-formats.readthedocs.io/en/latest/developers/logging.html#logging-frameworks)) and `slf4j-simple` which is normally taken from the classpath during runtime (but we would like to use GraalVM so include early).
+
+Visit https://central.sonatype.com to find these
+
+Currently these are:
+
+- https://central.sonatype.com/artifact/org.openmicroscopy/ome-xml/6.3.3
+- https://central.sonatype.com/artifact/org.openmicroscopy/ome-common/6.0.16
+- https://central.sonatype.com/artifact/org.slf4j/slf4j-simple/1.7.36
+- https://central.sonatype.com/artifact/org.slf4j/slf4j-api/1.7.36
+
+But we also need:
+- https://central.sonatype.com/artifact/org.openmicroscopy/ome-codecs/0.4.5
+
+so I copy the Maven snippets back-to-back:
+
+```
+<dependency>
+    <groupId>org.openmicroscopy</groupId>
+    <artifactId>ome-xml</artifactId>
+    <version>6.3.3</version>
+</dependency>
+<dependency>
+    <groupId>org.openmicroscopy</groupId>
+    <artifactId>ome-common</artifactId>
+    <version>6.0.16</version>
+</dependency>
+<dependency>
+    <groupId>org.slf4j</groupId>
+    <artifactId>slf4j-simple</artifactId>
+    <version>1.7.36</version>
+</dependency>
+<dependency>
+    <groupId>org.slf4j</groupId>
+    <artifactId>slf4j-api</artifactId>
+    <version>1.7.36</version>
+</dependency>
+<dependency>
+    <groupId>org.openmicroscopy</groupId>
+    <artifactId>ome-codecs</artifactId>
+    <version>0.4.5</version>
+</dependency>
+```
+
+and entering this to https://jar-download.com/online-maven-download-tool.php and downloading the project allows us to get the jars as well as the dependencies.
+
+We also need `formats-gpl.jar`, `formats-bsd.jar`, `formats-api.jar`, `turbojpeg.jar` from https://downloads.openmicroscopy.org/bio-formats/latest/artifacts/
+
+But we also need `org.scijava.nativelib.NativeLibraryUtil`, which is the native-lib-loader-2.4.0.jar from https://jar-download.com/artifacts/org.scijava/native-lib-loader/2.4.0
+
+But instead of this, we could have used 130% more space and used bioformats_package.jar from https://downloads.openmicroscopy.org/bio-formats/latest/artifacts/
+
+I added this jar nevertheless. Hence I sometimes remove `slf4j-simple.jar` against loggers competing.
+
+## Building
+
+```
+javac -cp ".:jar_files/*" org/camicroscope/BFBridge.java
+java -cp ".:jar_files/*" org/camicroscope/BFBridge
+```
+
+or run only:
+
+```
+java -cp ".:jar_files/*" org/camicroscope/BFBridge.java
+```
+
+for native-image:
+
+```
+javac -cp ".:jar_files/*" org/camicroscope/BFBridge.java
+native-image -cp ".:jar_files/*" --shared -H:Name=bfbridge org.camicroscope.BFBridge
+```
+
+## Optimization
+
+https://medium.com/graalvm/introducing-the-tracing-agent-simplifying-graalvm-native-image-configuration-c3b56c486271
+
+First created with `graalvm/java -cp ".:jar_files/*" -agentlib:native-image-agent=config-output-dir=META-INF/native-image org.camicroscope.BFBridge`
+
+Then used with `graalvm/java -cp ".:jar_files/*" -agentlib:native-image-agent=config-merge-dir=META-INF/native-image org.camicroscope.BFBridge`
+
+## Loading resources
+
+https://www.graalvm.org/latest/reference-manual/native-image/dynamic-features/Resources/
+
+I got:
+
+```
+[main] WARN loci.formats.ClassList - Could not find readers.txt
+```
+
+and this is one of the files bundled inside one of bioformats jars. These are accessed through getResourceAsStream() and searching for this in bioformats repo reveals these.
+
+So I edited resource-config.json and added -H:Log=registerResource:3 to compilation
+
+I took the inclusive bioformats_package.jar and decompressed it.
+
+Also, this is OK:
+
+```
+Warning: Could not resolve class loci.formats.in.ScreenReader for reflection configuration. Reason: java.lang.ClassNotFoundException: loci.formats.in.ScreenReader.
+Warning: Could not resolve class loci.formats.in.SlideBook6Reader for reflection configuration. Reason: java.lang.ClassNotFoundException: loci.formats.in.SlideBook6Reader.
+Warning: Could not resolve class loci.formats.in.URLReader for reflection configuration. Reason: java.lang.ClassNotFoundException: loci.formats.in.URLReader.
+Warning: Could not resolve class loci.formats.in.ZarrReader for reflection configuration. Reason: java.lang.ClassNotFoundException: loci.formats.in.ZarrReader.
+```
+
+These are optional classes we won't need.
+
+But if you get:
+
+```
+Exception in thread "main" java.lang.UnsatisfiedLinkError: no awt in java.library.path
+```
+
+native-image already said: AWT:  Use the tracing agent to collect metadata for AWT.
+
+What to do?
+
+Is it https://github.com/graalvm/mandrel/issues/487 referring to fixed in JDK 21?
---
+++ org/camicroscope/BFBridge.java
@@ -0,0 +1,287 @@
+package org.camicroscope;
+
+import loci.formats.IFormatReader;
+import loci.formats.ImageReader;
+import loci.formats.FormatTools; // https://downloads.openmicroscopy.org/bio-formats/latest/api/loci/formats/FormatTools.html
+import static org.graalvm.nativeimage.c.type.CTypeConversion.toCString; // Watch out for memory leaks
+import static org.graalvm.nativeimage.c.type.CTypeConversion.toCBytes; // Likewise
+import static org.graalvm.nativeimage.c.type.CTypeConversion.toCBoolean;
+import static org.graalvm.nativeimage.c.type.CTypeConversion.toJavaString;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.word.WordFactory;
+
+// TODO: we return error messages in C pointers; how to free these from C?
+// We might also want to use a try-let before we use .get() on returning strings
+
+public class BFBridge {
+
+    // TODO: consider providing more metadata
+    // https://bio-formats.readthedocs.io/en/latest/metadata-summary.html
+
+    private static IFormatReader reader = new ImageReader();
+    private static String lastError = "";
+    private static byte[] readBuffer = new byte[10000000];
+
+    @CEntryPoint
+    static CCharPointer BFGetError(IsolateThread t) {
+        return toCString(lastError).get();
+    }
+
+    @CEntryPoint
+    static byte BFOpen(IsolateThread t, CCharPointer filePath) {
+        try {
+            // Use the easier resolution API
+            reader.setFlattenedResolutions(false);
+            reader.setId(toJavaString(filePath));
+            return toCBoolean(true);
+        } catch (Exception e) {
+            lastError = e.toString();
+            return toCBoolean(false);
+        }
+    }
+
+    // If expected to be the single file, or always true for single-file formats
+    @CEntryPoint
+    static byte BFIIsSingleFile(IsolateThread t, CCharPointer filePath) {
+        try {
+            return toCBoolean(reader.isSingleFile(toJavaString(filePath)));
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static byte BFClose(IsolateThread t, int fileOnly) {
+        try {
+            reader.close(fileOnly != 0);
+            return toCBoolean(true);
+        } catch (Exception e) {
+            lastError = e.toString();
+            return toCBoolean(false);
+        }
+    }
+
+    @CEntryPoint
+    static int BFGetResolutionCount(IsolateThread t) {
+        try {
+            // In resolution mode, each of series has a number of resolutions
+            // WSI pyramids have multiple and others have one
+            // This method returns resolution counts for the current series
+            return reader.getResolutionCount();
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static byte BFSetCurrentResolution(IsolateThread t, int resIndex) {
+        try {
+            // Precondition: The caller must check that at least 0 and less than
+            // resolutionCount
+            reader.setResolution(resIndex);
+            return toCBoolean(true);
+        } catch (Exception e) {
+            lastError = e.toString();
+            return toCBoolean(false);
+        }
+    }
+
+    @CEntryPoint
+    static int BFGetSizeX(IsolateThread t) {
+        try {
+            // For current resolution
+            return reader.getSizeX();
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static int BFGetSizeY(IsolateThread t) {
+        try {
+            return reader.getSizeY();
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static int BFGetOptimalTileWidth(IsolateThread t) {
+        try {
+            return reader.getOptimalTileWidth();
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static int BFGetOptimalTİleHeight(IsolateThread t) {
+        try {
+            return reader.getOptimalTileHeight();
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static CCharPointer BFGetFormat(IsolateThread t) {
+        try {
+            return toCString(reader.getFormat()).get();
+        } catch (Exception e) {
+            lastError = e.toString();
+            return toCString("").get();
+        }
+    }
+
+    @CEntryPoint
+    static int BFGetPixelType(IsolateThread t) {
+        try {
+            return reader.getPixelType();
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static int BFGetBitsPerPixel(IsolateThread t) {
+        try {
+            return reader.getBitsPerPixel();
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static int BFGetBytesPerPixel(IsolateThread t) {
+        try {
+            return FormatTools.getBytesPerPixel(reader.getPixelType());
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static int BFGetRGBChannelCount(IsolateThread t) {
+        try {
+            return reader.getRGBChannelCount();
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static byte BFIsRGB(IsolateThread t) {
+        try {
+            return toCBoolean(reader.isRGB());
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static byte BFIsInterleaved(IsolateThread t) {
+        try {
+            return toCBoolean(reader.isInterleaved());
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static byte BFIsLittleEndian(IsolateThread t) {
+        try {
+            return toCBoolean(reader.isLittleEndian());
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static byte BFIsFloatingPoint(IsolateThread t) {
+        try {
+            return toCBoolean(FormatTools.isFloatingPoint(reader));
+        } catch (Exception e) {
+            lastError = e.toString();
+            return -1;
+        }
+    }
+
+    @CEntryPoint
+    static CCharPointer BFIGetDimensionOrder(IsolateThread t) {
+        try {
+            return toCString(reader.getDimensionOrder()).get();
+        } catch (Exception e) {
+            lastError = e.toString();
+            // TODO: use null pointer
+            return toCString("").get();
+        }
+    }
+
+    // TODO: verify that returning null pointer is OKAY
+    // TODO: return a 10MB array 100 times and see if there are memory leaks
+    @CEntryPoint
+    static CCharPointer BFIOpenBytes(IsolateThread t, int x, int y, int w, int h) {
+        try {
+            int size = w * h * FormatTools.getBytesPerPixel(reader.getPixelType()) * reader.getRGBChannelCount();
+            if (size > readBuffer.length) {
+                lastError = "Requested tile too big; must be at most " + readBuffer.length + " bytes";
+                return toCString("").get();
+            }
+            // TODO: do we need to handle other planes here?
+            reader.openBytes(0, readBuffer, x, y, w, h);
+            // Erase previous responses
+            for (int i = size; i < readBuffer.length; i++) {
+                readBuffer[i] = 0;
+            }
+            return toCBytes(readBuffer).get();
+        } catch (Exception e) {
+            lastError = e.toString();
+            // This is permitted: https://github.com/oracle/graal/blob/492c6016c5d9233be5de2dd9502cc81f746fc8e7/substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/c/CTypeConversionSupportImpl.java#L55
+            return WordFactory.nullPointer();
+        }
+    }
+
+    // Debug function
+    public static byte openFile(String filename) throws Exception {
+        try {
+                        System.out.println("start 1");
+            reader.close(true);
+                                    System.out.println("start 2");
+
+            reader.setId("/Users/zerf/Downloads/Github-repos/CGDogan/camic-Distro/images/posdebugfiles_2.dcm");
+                                    System.out.println("start 3");
+
+            reader.openBytes(0);
+                                    System.out.println("start 4");
+
+            return 0;
+        } catch (Exception e) {
+            System.out.println("excepting incoming");
+            throw e;
+        }
+    }
+
+    public static void main(String args[]) throws Exception {
+        int x = 10;
+        int y = 25;
+        int z = x + y;
+
+        openFile("");
+        // System.out.println("Sum of x+y = " + z);
+    }
+}
